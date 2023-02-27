aff19747a861f63ca188bdbb84e5345e78993396
Vladimir Ventura Furcal <vladventura@outlook.es>
2023-02-27 11:44:53 -0500

    Small platform safeties
    Signed-off-by: Vladimir Ventura Furcal <vladventura@outlook.es>

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,25 @@ final dylibPath =
     path.join(Directory.current.absolute.path, 'set_wallpaper.so');
 final nl = NativeLibrary(ffi.DynamicLibrary.open(dylibPath));
 
+Future<String> normalizeExistsPath(String input) async {
+  String normalized = path.normalize(input);
+  if (Platform.isWindows && !normalized.contains("C:")) {
+    normalized = "C:\\$normalized";
+  }
+  if (await File(normalized).exists()) {
+    return normalized;
+  }
+  return "";
+}
+
+Future<void> setWallpaperWindows(String input) async {
+  if (input.isEmpty) return;
+  String pathToFile = await normalizeExistsPath(input);
+  ffi.Pointer<ffi.Char> charP = pathToFile.toNativeUtf8().cast<ffi.Char>();
+  nl.change_wallpaper(charP);
+  malloc.free(charP);
+}
+
 void main() {
   debugPrint(Directory.current.absolute.path);
   int result = nl.set_wallpaper(10, 20);
@@ -45,7 +64,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _input = "";
 
-  void pickFile() async {
+  void _pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       dialogTitle: "Pick a Wallpaper!",
       type: FileType.image,
@@ -57,32 +76,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future<String> normalizePath(String input) async {
-    String normalized = path.normalize(input);
-    if (Platform.isWindows && !normalized.contains("C:")) {
-      normalized = "C:\\$normalized";
-    }
-    if (await File(normalized).exists()) {
-      return normalized;
-    }
-    return "";
-  }
-
   Future<void> _setWallpaper() async {
-    if (_input.isEmpty) return;
-    String pathToFile = await normalizePath(_input);
-    ffi.Pointer<ffi.Char> charP = pathToFile.toNativeUtf8().cast<ffi.Char>();
-    nl.change_wallpaper(charP);
-    malloc.free(charP);
+    if (Platform.isWindows) {
+      setWallpaperWindows(_input);
+    }
   }
 
-  void setInput(String s) {
-    setState(() {
-      _input = s;
-    });
-  }
-
-  void onSubmit() async {
+  void _changeWallpaper() async {
     await _setWallpaper();
     debugPrint(_input);
   }
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: pickFile,
+              onPressed: _pickFile,
               child: const Text("Open File"),
             ),
             if (_input.isNotEmpty) Text("Path to file is $_input"),
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: onSubmit,
+        onPressed: _changeWallpaper,
         tooltip: 'Set Wallpaper',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
