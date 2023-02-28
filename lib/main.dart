948278d7fa6bd24851a65a9160c343b36556391f
Vladimir Ventura <vladventura@outlook.es>
2023-02-28 15:48:59 -0500

    Sadly, we must return to the original working directory
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,9 @@
-import 'dart:async';
-import 'dart:ffi' as ffi;
-import 'dart:io' show Directory, File, Platform;
-import 'package:ffi/ffi.dart';
+import "dart:io" show Directory;
+import 'package:ambience/handlers/wallpaper_handler.dart';
 import 'package:flutter/material.dart';
-import 'package:ambience/native/generated_bindings.dart';
-import 'package:path/path.dart' as path;
 import 'package:file_picker/file_picker.dart';
 
-final dylibPath =
-    path.join(Directory.current.absolute.path, 'set_wallpaper.so');
-final nl = NativeLibrary(ffi.DynamicLibrary.open(dylibPath));
-
-Future<String> normalizeExistsPath(String input) async {
-  String normalized = path.normalize(input);
-  // if (Platform.isWindows && !normalized.contains("C:")) {
-  //   normalized = "C:\\$normalized";
-  // }
-  if (await File(normalized).exists()) {
-    return normalized;
-  }
-  return "";
-}
-
-Future<void> setWallpaperWindows(String input) async {
-  if (input.isEmpty) return;
-  String pathToFile = await normalizeExistsPath(input);
-  ffi.Pointer<ffi.Char> charP = pathToFile.toNativeUtf8().cast<ffi.Char>();
-  nl.change_wallpaper(charP);
-  malloc.free(charP);
-}
-
 void main() {
-  debugPrint(Directory.current.absolute.path);
-  int result = nl.set_wallpaper(10, 20);
-  debugPrint(result.toString());
   runApp(const MyApp());
 }
 
@@ -65,10 +35,12 @@ class _MyHomePageState extends State<MyHomePage> {
   String _input = "";
 
   void _pickFile() async {
+    String currentWorkingDir = Directory.current.absolute.path;
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       dialogTitle: "Pick a Wallpaper!",
       type: FileType.image,
     );
+    Directory.current = currentWorkingDir;
     if (result == null) return;
     PlatformFile file = result.files.single;
     setState(() {
@@ -76,16 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future<void> _setWallpaper() async {
-    if (Platform.isWindows) {
-      setWallpaperWindows(_input);
-    }
-  }
-
-  void _changeWallpaper() async {
-    await _setWallpaper();
-    debugPrint(_input);
-  }
+  void _setWallpaper() async => await WallpaperHandler.setWallpaper(_input);
 
   @override
   Widget build(BuildContext context) {
@@ -106,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _changeWallpaper,
+        onPressed: _setWallpaper,
         tooltip: 'Set Wallpaper',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
