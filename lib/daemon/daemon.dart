92b90df36f7e10a46792b1188de9fe6890cc93b6
Bryan Tait <bryan.b.tait@gmail.com>
2023-03-11 19:07:33 -0500

    Added Linux Daemon Prototype
    Also fixed a minor bug with passing arguments to both linux and windows daemon scripts

--- lib/daemon/daemon.dart
+++ lib/daemon/daemon.dart
@@ -5,14 +5,20 @@ class Daemon {
   //time format: xx:xxam or xx:xxpm e.g. 10:01am or 10:01pm.
   //Note: can do just do xx<am or pm> e.g. 10am
   static void init(String? cityName, String time) async {
+
+    String current = Directory.current.path;
     if (cityName == null) {
       return;
     }
     if (Platform.isWindows) {
-      String current = Directory.current.path;
-      var proc = await Process.run('powershell.exe',
-          [' $current\\winTaskSetter.ps1 $cityName $time']);
-      debugPrint("proc results: ${proc.stderr}");
+      var proc = await Process.run(
+          'powershell.exe', [' $current\\winTaskSetter.ps1 "$cityName" $time']);
+      debugPrint("process standard error output: ${proc.stderr}");
+    }
+    //notice: Requires Linux command 'xvfb' to be installed function
+    else if (Platform.isLinux) {
+      var proc = await Process.run('bash', ['-c', '$current/UbuntuCronScheduler.sh "$cityName"']);
+      debugPrint("process standard error output: ${proc.stderr}");
     } else {
       debugPrint(
           "Other platform implementions are not included in this prototype atm");
