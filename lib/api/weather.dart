cfa39cec43eafcf2b2e7d8d89e44e0d1a24143ee
Vladimir Ventura <vladventura@outlook.es>
2023-03-15 09:17:58 -0400

    Changed called function
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- lib/api/weather.dart
+++ lib/api/weather.dart
@@ -1,14 +1,17 @@
 import 'weather_api.dart';
 import 'package:flutter/foundation.dart';
 import 'package:ambience/storage/storage.dart';
+import 'dart:developer';
+
 //Reads weather data JSON from file.
 //Note this is a bit if a proof of concept function
 //Data needs to be prased to be utitlize, right now there is only an example of doing so
 void weather(String? input) async {
   //not fully functional
   //get weather data
-  if (await (getWeather(input)) == false) {
+  if (await (getAndWriteWeather(input)) == false) {
     debugPrint("Failed to get Weatherdata!");
+    return;
   }
   //else successfuly gotten weather data
   var storage = Storage();
@@ -16,6 +19,7 @@ void weather(String? input) async {
   var weatherData = await storage.readAppDocJson(storage.weatherDataPath);
   //sample of parsing weather data
   //note it is a mix of lists and maps. Hence key and index accesing.
+  inspect(weatherData);
   debugPrint(
       "Found weather for ${weatherData['city']['name']} in ${weatherData['city']['country']}."
       " The weather: ${(weatherData['list'][0])['weather'][0]['description']}.");
