c656bdb41bdaef76f628eb347dd29aac853ef414
Vladimir Ventura <vladventura@outlook.es>
2023-03-29 18:05:24 -0400

    Adding tests for Storage, and asserting scripts exist
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,16 +1,20 @@
 import "dart:convert";
-import "dart:io" show File;
-import "package:ambience/api/geolocate_api.dart";
-import "package:ambience/api/weather_api.dart";
-import "package:ambience/handlers/request_handler.dart";
-import "package:ambience/models/geolocation_model.dart";
-import "package:ambience/models/weather_model.dart";
+import "dart:io" show File, Directory, FileSystemEntity;
+import "package:ambience/constants.dart";
+import "package:ambience/storage/storage.dart";
 import "package:flutter/foundation.dart";
 import 'package:path/path.dart' as path;
-import "package:flutter_test/flutter_test.dart";
-import "package:ambience/handlers/wallpaper_handler.dart";
 import 'package:http/http.dart' as http;
+import "package:flutter/services.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:ambience/api/geolocate_api.dart";
+import "package:ambience/api/weather_api.dart";
+import "package:ambience/handlers/file_handler.dart" as FileHandler;
+import "package:ambience/handlers/request_handler.dart";
+import "package:ambience/handlers/wallpaper_handler.dart";
+import "package:ambience/models/geolocation_model.dart";
+import "package:ambience/models/weather_model.dart";
 
 import "mock/mock_data.dart";
 
@@ -81,15 +85,62 @@ void main() {
       modelFromJson = GeolocationModel.fromJson(geolocationDataListItem);
       expect(modelFromJson.lat, geolocationDataListItem['lat']);
     });
-    test("Should allow to late initialize city name", (){
+    test("Should allow to late initialize city name", () {
       modelFromJson = GeolocationModel.fromJson(geolocationDataListItem);
       modelFromJson.cityName = fakeCityInfo['cityName'];
       expect(modelFromJson.cityName, fakeCityInfo['cityName']);
     });
-      test("Should allow to late initialize country code", (){
+    test("Should allow to late initialize country code", () {
       modelFromJson = GeolocationModel.fromJson(geolocationDataListItem);
       modelFromJson.countryCode = fakeCityInfo['countryCode'];
       expect(modelFromJson.countryCode, fakeCityInfo['countryCode']);
+    });
+  });
+  group('Storage', () {
+    test('Should take weather data path from constants file', () {
+      Storage storage = Storage();
+      expect(storage.weatherDataPath, weatherDataPath);
+    });
+    test('Should take log file path from constants file', () {
+      Storage storage = Storage();
+      expect(storage.logFilePath, logFilePath);
+    });
+    test('Should take config file path from constants file', () {
+      Storage storage = Storage();
+      expect(storage.configPath, configPath);
+    });
+    test('Should take app\'s data directory name from constants file', () {
+      Storage storage = Storage();
+      expect(storage.appDataDirName, appDataDirName);
+    });
+  });
+  group('Scripts', () {
+    Directory scriptsDir;
+    late List<FileSystemEntity> allFiles;
+    setUp(() async {
+      scriptsDir = Directory(
+          path.normalize(path.join(Directory.current.path, 'scripts')));
+      allFiles = await scriptsDir.list().toList();
+    });
+    test('Should find Windows scripts', () async {
+      bool found = false;
+      for (FileSystemEntity fse in allFiles) {
+        if (fse.path.contains('win')) {
+          found = true;
+          break;
+        }
+      }
+      expect(found, true);
+    });
+    test('Should find Ubuntu scripts', () async {
+      bool found = false;
+      for (FileSystemEntity fse in allFiles) {
+        if (fse.path.contains('Ubuntu')) {
+          found = true;
+          break;
+        }
+      }
+      expect(found, true);
     });
   });
 }
