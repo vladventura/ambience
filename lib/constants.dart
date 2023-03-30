df2eb18818a6bf4298f52de915a75dc5fe3a8b73
Vladimir Ventura <vladventura@outlook.es>
2023-03-29 22:22:27 -0400

    Adding jsonPath to constants file
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ const String weatherDataPath = "weatherData.json";
 const String logFilePath = "log.txt";
 const String configPath = "config.txt";
 const String appDataDirName = "Ambience";
+const String jsonPath = "ruleset.json";
