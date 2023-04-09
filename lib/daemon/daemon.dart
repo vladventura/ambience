af8bfab123734bc2a7c331bf33e3fca9a196e7dd
Bryan Tait <bryan.b.tait@gmail.com>
2023-04-08 21:37:36 -0400

    Daemons now throw errors

--- lib/daemon/daemon.dart
+++ lib/daemon/daemon.dart
@@ -4,33 +4,12 @@ import "package:ambience/models/weather_model.dart";
 import "package:ambience/storage/storage.dart";
 import "package:ambience/weatherEntry/weather_entry.dart";
 import "package:flutter/material.dart";
-import 'package:workmanager/workmanager.dart';
 import 'package:ambience/handlers/wallpaper_handler.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:ambience/api/weather_api.dart';
 
 @pragma(
     'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
-/*
-void callbackDispatcher() {
-  //determine time offset for android, move to own function later
-
-  Workmanager().executeTask((task, inputData) async {
-    switch (task) {
-      case "ambience_daemon":
-        print("Ambience daemon triggered!");
-        String city = inputData?['city'];
-        String targetWeather = inputData?['targetWeather'];
-        String input = inputData?['wallpaper'];
-        if (input != 'null') {
-          await WallpaperHandler.setWallpaper(input);
-        }
-        break;
-    }
-
-    return Future.value(true);
-  });
-}
-*/
 
 //Visual feedback for Android testing
 void wallpaperChangeTest(int id, Map params) async {
@@ -80,6 +59,11 @@ class Daemon {
     String current = Directory.current.path;
     String daemonMode = 'boot';
     if (Platform.isWindows) {
+      File checkExist = File("$current\\winTaskSetter.ps1");
+      //check if script exists
+      if (!(await checkExist.exists())) {
+        throw ("winTaskSetter.ps1 does not exist");
+      }
       var proc = await Process.run('PowerShell.exe', [
         '-ExecutionPolicy',
         'Bypass',
@@ -90,13 +74,25 @@ class Daemon {
       ]);
       debugPrint("winTaskSetter.ps1 standard output: ${proc.stdout}");
       debugPrint("winTaskSetter.ps1 standard error output: ${proc.stderr}");
+
+      if (proc.exitCode == 0) {
+        throw "winTaskSetter.ps1 did not execute successfully";
+      }
     } else if (Platform.isLinux) {
+      File checkExist = File("$current/UbuntuCronScheduler.sh");
+      //check if script exists
+      if (!(await checkExist.exists())) {
+        throw "UbuntuCronScheduler.sh does not exist";
+      }
       var proc = await Process.run('bash', [
         '-c',
         '$current/UbuntuCronScheduler.sh "$daemonMode" "$bootDaemonID"'
       ]);
       debugPrint(
           "UbuntuCronScheduler.sh standard error output: ${proc.stderr}");
+      if (proc.exitCode == 0) {
+        throw ("UbuntuCronScheduler.sh did not execute successfully");
+      }
     } else if (Platform.isAndroid) {
       debugPrint("Android boot daemon is not implemented yet!");
     }
@@ -113,6 +109,10 @@ class Daemon {
     //daemon mode is always normal for daemonspawner
     const String daemonMode = 'n';
     if (Platform.isWindows) {
+      File checkExist = File("'$current\\winTaskSetter.ps1'");
+      if (!(await checkExist.exists())) {
+        throw ("winTaskSetter.ps1 does not exist");
+      }
       //Turn time in a 24 hour formatted string that is acceptable by task scheduler command
       String formatedTime =
           '${ruleTime.hour.toString().padLeft(2, '0')}:${ruleTime.hour.toString().padLeft(2, '0')}';
@@ -131,13 +131,26 @@ class Daemon {
 
       debugPrint("winTaskSetter.ps1 standard output: ${proc.stdout}");
       debugPrint("winTaskSetter.ps1 standard error output: ${proc.stderr}");
+      //check if script executed successfully
+      if (proc.exitCode == 0) {
+        throw "winTaskSetter.ps1 did not execute successfully";
+      }
     } else if (Platform.isLinux) {
+      File checkExist = File("$current/UbuntuCronScheduler.sh");
+      //check if script exists
+      if (!(await checkExist.exists())) {
+        throw "UbuntuCronScheduler.sh does not exist";
+      }
       var proc = await Process.run('bash', [
         '-c',
         '$current/UbuntuCronScheduler.sh "$daemonMode" "$id" ${ruleTime.hour} ${ruleTime.minute} $dow'
       ]);
       debugPrint(
           "UbuntuCronScheduler.sh standard error output: ${proc.stderr}");
+
+      if (proc.exitCode == 0) {
+        throw "UbuntuCronScheduler.sh did not execute successfully";
+      }
     } else if (Platform.isAndroid) {
       debugPrint("In android case");
       await AndroidAlarmManager.initialize();
@@ -149,36 +162,6 @@ class Daemon {
           startAt: minutefuture,
           rescheduleOnReboot: true,
           params: {"wallpaper": wallpaperpath});
-
-/* Android work manager code[currently replaced by AndroidAlarmManager]
-      Workmanager().initialize(
-          callbackDispatcher, // The top level function, aka callbackDispatcher
-          isInDebugMode:
-              true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-          );
-
-      //android tasks are timer base, so calculate offset from now and ruletime
-      Duration offset = calcTimeOffset(ruleTime, dow);
-      //for testing only!!!!!
-      offset = const Duration(minutes: 1);
-      if (offset == Duration.zero) {
-        //if offset is 0, register this task, for this time, every week.
-        Workmanager().registerPeriodicTask(id, "ambience_daemon",
-            frequency: const Duration(days: 7),
-            inputData: {
-              "city": city,
-              "targetWeather": ruleObj.weatherCondition.name
-            });
-      }
-      //else it's a one offtask
-      Workmanager().registerOneOffTask(id, "ambience_daemon",
-          initialDelay: offset,
-          inputData: {
-            "city": city,
-            "targetWeather": ruleObj.weatherCondition.name,
-            'wallpaper': wallpaperpath
-          });
-  */
     } else {
       debugPrint("Platform not supported");
     }
@@ -188,6 +171,11 @@ class Daemon {
   static void daemonBanisher(String idSchema) async {
     String current = Directory.current.path;
     if (Platform.isWindows) {
+      File checkExist = File("$current\\winTaskRemover.ps1");
+      //check if script exists
+      if (!(await checkExist.exists())) {
+        throw "winTaskRemover.ps1 does not exist";
+      }
       var proc = await Process.run('PowerShell.exe', [
         '-ExecutionPolicy',
         'Bypass',
@@ -198,12 +186,23 @@ class Daemon {
 
       debugPrint("winTaskRemover.ps1 standard output: ${proc.stdout}");
       debugPrint("winTaskRemover.ps1 standard error output: ${proc.stderr}");
+      if (proc.exitCode == 0) {
+        throw ("winTaskRemover.ps1 did not execute successfully");
+      }
     } else if (Platform.isLinux) {
+      File checkExist = File("$current/UbuntuCronRemover.sh");
+      //check if script exists
+      if (!(await checkExist.exists())) {
+        throw "UbuntuCronRemover.sh does not exist";
+      }
       var proc = await Process.run(
           'powershell.exe', ['$current/UbuntuCronRemover.sh "$idSchema"']);
 
       debugPrint("UbuntuCronRemover.sh standard output: ${proc.stdout}");
       debugPrint("UbuntuCronRemover.sh standard error output: ${proc.stderr}");
+      if(proc.exitCode == 0){
+          throw "UbuntuCronRemover.sh does not execute successfully";
+      }
     } else if (Platform.isAndroid) {
       debugPrint("Android daemonBanisher not implemented yet");
       AndroidAlarmManager.cancel(0);
@@ -214,21 +213,37 @@ class Daemon {
 
   //checks for desired weather condition and changes wallpaper if it is.
   static void weatherCheck(WeatherEntry ruleObj) async {
+    //if cannot access weather api, use offline data
+    if ((await getAndWriteWeather(ruleObj.city)) == false) {
+      debugPrint(
+          "Cannot access online weather data, checking offline weather data. -Daemon.weathercheck");
+    }
     //might want to pull the weather parsing, and finding most current as it's own function or addition to some other handler.
     Storage store = Storage();
     var weatherJson = await (store.readAppDocJson(weatherDataPath));
+    if (weatherJson == 'failed') {
+      debugPrint("Cannot read weatherData.JSON,exiting -Daemon.weathercheck");
+      //at this point the daemon cannot do it's job
+      throw "Cannot access online or offline weatherData";
+    }
     //get the list of maps of weather data from the JSON
     List<dynamic> respDecode = weatherJson['list'];
     //Take the map from each list element and parse using Weathermodel
     List<WeatherModel> allWeatherData =
         respDecode.map((e) => WeatherModel.fromJson(e)).toList();
-    //to-do, add backup to parse through offline data and locate up to date data.
-    //if current weather condition matches desired weather condition, change wallpaper
-    for (int i = 0; i < allWeatherData.length; i++) {
-      bool match = ruleObj.compareWeather(allWeatherData[i].weathers[0].name);
-      if (match) {
-        WallpaperHandler.setWallpaper(ruleObj.wallpaperFilepath);
+    int i;
+    //find most up to date weather data
+    for (i = 0; i < allWeatherData.length; i++) {
+      var timeStamp = DateTime.parse(allWeatherData[i].datetimeText).toLocal();
+      //stop once, most up to date weather is found
+      if (!(DateTime.now().isAfter(timeStamp))) {
+        break;
       }
+    }
+    //check if wallpaper change is needed
+    bool match = ruleObj.compareWeather(allWeatherData[i].weathers[0].name);
+    if (match) {
+      WallpaperHandler.setWallpaper(ruleObj.wallpaperFilepath);
     }
   }
 }
