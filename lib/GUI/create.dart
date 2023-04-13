d681e536cde42c6e51d056abd0f7e87f11b4a0ac
mishta-bean <etighe238@gmail.com>
2023-04-12 22:13:18 -0400

    Revert "Merge branch 'Max's-GUI-Branch' of https://github.com/vladventura/ambience into Max's-GUI-Branch"
    This reverts commit eef784f24bc8feba1705ae1bb47bc679cf45f371, reversing
    changes made to b273ebfcbab127ace69f6f1833ac75eca2d939dd.

--- lib/GUI/create.dart
+++ lib/GUI/create.dart
@@ -9,10 +9,8 @@ import 'dart:io';
 import "package:ambience/GUI/list.dart";
 import "package:ambience/handlers/file_handler.dart";
 
-void main() => runApp(CreateApp(
-      contextWallpaper: WallpaperObj(),
-      intention: 1,
-    ));
+void main() => runApp(CreateApp(contextWallpaper: WallpaperObj(), intention: 1,));
+
 
 // Global variables
 
@@ -27,17 +25,18 @@ final ButtonStyle dayToggleButton = OutlinedButton.styleFrom(
 
 // end of globals
 
+
 class WeatherDropMenu extends StatefulWidget {
   const WeatherDropMenu({super.key});
 
-  @override
+ @override
   State<WeatherDropMenu> createState() => _WeatherDropMenuState();
 }
 
 class WallpaperChooser extends StatefulWidget {
   WallpaperObj chosen;
 
-  WallpaperChooser({Key? key, required this.chosen}) : super(key: key);
+  WallpaperChooser({ Key? key, required this.chosen}): super(key: key);
 
   @override
   State<WallpaperChooser> createState() => _WallpaperChooser();
@@ -46,79 +45,61 @@ class WallpaperChooser extends StatefulWidget {
 class AmPmToggle extends StatefulWidget {
   const AmPmToggle({super.key});
 
-  @override
+ @override
   State<AmPmToggle> createState() => _AmPmToggle();
 }
 
-String setWeatherCond(IconData cond) {
-  //converts icon's data to a string
-  return cond.toString();
+String setWeatherCond(IconData cond) { //converts icon's data to a string
+  return cond.toString(); 
 }
 
 Future<String> chooseFile() async {
   try {
     var filepath = await getImagePathFromPicker();
     return filepath;
-  } on NoFileChosenException {
+  } on NoFileChosenException{
     return "";
-  } on FileNotFoundException {
+  } on FileNotFoundException{
     return "";
-  } catch (e) {
+  } catch(e){
     return "";
   }
 }
-
-Widget checkWallpaper(String str) {
-
-  if(File(str).existsSync()) {
-    return Expanded(
-      child: Image.file(
-        File(str),
-        fit: BoxFit.fitHeight,), // placeholder, retrieve wallpaper image here
-    );
-  } else {
-    return Container(
-            alignment: Alignment.center,
-            child:Expanded(
-            child: const Text("\t No wallpaper currently displayed \t"),
-            )
-          );
-  }
-}
-
 
 class _WeatherDropMenuState extends State<WeatherDropMenu> {
+      
   List<IconData> weathers = <IconData>[
-    Icons.sunny,
+    Icons.sunny, 
     Icons.cloud,
-    Icons.water_drop,
-    Icons.thunderstorm,
-    Icons.cloudy_snowing,
+    Icons.shower,
+    Icons.thunderstorm, 
+    Icons.cloudy_snowing, 
   ];
 
   IconData weatherVal = Icons.hourglass_bottom;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // needs a "stateful widget" to work properly and change states
-        alignment: Alignment.topCenter,
-        padding: EdgeInsets.only(right: 24),
-        child: DropdownButton<IconData>(
-          icon: Icon(weatherVal), //placeholder icon value for now
-          iconSize: 64,
-          onChanged: (IconData? newVal) {
-            setState(() {
-              weatherVal = newVal!;
-            });
-          }, // update current weather value
-          items: weathers.map<DropdownMenuItem<IconData>>((IconData value) {
-            return DropdownMenuItem<IconData>(
-              value: value,
-              child: Icon(value),
+    return Container( // needs a "stateful widget" to work properly and change states
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(right: 24),
+            child: DropdownButton<IconData>(
+              icon: Icon(weatherVal), //placeholder icon value for now
+              iconSize: 64,
+              onChanged: (IconData? newVal) {
+                setState(() {
+                  weatherVal = newVal!;
+                });
+              }, // update current weather value
+              items: weathers.map<DropdownMenuItem<IconData>>((IconData value) {
+                return DropdownMenuItem<IconData>(
+                  value: value,
+                  child: Icon(value),
             );
-          }).toList(),
-        ));
+          }
+        ).toList(),
+      )
+    );
   }
 }
 
@@ -126,55 +107,63 @@ class _WallpaperChooser extends State<WallpaperChooser> {
 
   @override
   Widget build(BuildContext context) {
-    Widget img = checkWallpaper(widget.chosen.filePath);
+
+    Widget img = Image.file(
+        File(widget.chosen.filePath),
+        fit: BoxFit.fitHeight,);
 
     return Expanded(
-      child: InkWell(
-        onTap: () async {
-          // updates the wallpaper to be the newly selected image
-          widget.chosen.filePath = await chooseFile();
-          imageCache.clear();
-          imageCache.clearLiveImages();
-          checkWallpaper(widget.chosen.filePath);
-          setState(() {});
-        },
-        child: Container(
-            child: img,
-        )),
-    );
+    child: IconButton( // placeholder, retrieve wallpaper image here
+      icon: img,
+        onPressed: () async { // updates the wallpaper to be the newly selected image
+        widget.chosen.filePath = await chooseFile();
+        imageCache.clear();
+        imageCache.clearLiveImages();
+        img = Image.file(
+        File(widget.chosen.filePath),
+        fit: BoxFit.fitHeight,);
+        setState(() {});
+      }, //open file explorer here
+    ),
+  );
   }
 }
 
 class _AmPmToggle extends State<AmPmToggle> {
+
+
   bool amPm = true; // true = AM, false = PM
 
   Text txt = Text("A.M.", style: TextStyle(fontSize: 11));
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+
     return Container(
-        height: 80,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 2),
-        ),
-        child: OutlinedButton(
-          onPressed: () {
-            if (amPm == true) {
-              setState(() {
-                txt = Text("P.M.", style: TextStyle(fontSize: 11));
-                amPm = false;
-              });
-            } else if (amPm == false) {
-              setState(() {
-                txt = Text("A.M.", style: TextStyle(fontSize: 11));
-                amPm = true;
-              });
-            }
-          }, // switches from A.M. to P.M. and vice versa on click
-          child: txt,
-          style: dayToggleButton,
-        ));
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+              ),
+
+              child: OutlinedButton(onPressed: (){
+                if(amPm == true){
+                  setState(() {
+                    txt = Text("P.M.", style: TextStyle(fontSize: 11));
+                    amPm = false;
+                  });
+                }
+                else if(amPm == false){
+                  setState(() {
+                    txt = Text("A.M.", style: TextStyle(fontSize: 11));
+                    amPm = true;
+                  });
+                }
+              }, // switches from A.M. to P.M. and vice versa on click
+                child: txt,
+                style: dayToggleButton,
+          ));
   }
+
 }
 
 class CreateMsg extends StatelessWidget {
@@ -236,10 +225,10 @@ class CreateApp extends StatefulWidget {
 
   final WallpaperObj contextWallpaper;
 
-  // 1 = create new wallpaper,
-  // 2 = copy from existing wallpaper,
+  // 1 = create new wallpaper, 
+  // 2 = copy from existing wallpaper, 
   // 3 = edit existing wallpaper
-  final int intention;
+  final int intention; 
 
  @override
   State<CreateApp> createState() => _CreateApp();
@@ -248,11 +237,8 @@ class CreateApp extends StatefulWidget {
 class _CreateApp extends State<CreateApp> {
 
   bool isNumeric(String s) {
-    if (s == null) {
-      return false;
-    }
-
-    return int.tryParse(s) != null;
+  if(s == null) {
+    return false;
   }
 
   return int.tryParse(s) != null;
@@ -294,7 +280,7 @@ class _CreateApp extends State<CreateApp> {
 
         break;
 
-      case 2:
+      case 2: 
 
         // use newObj, create new wallpaper entry(s) with it,
         // keep original wallpaper entry(s) intact.
@@ -316,16 +302,15 @@ class _CreateApp extends State<CreateApp> {
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
 
     bool _visibleErr = false;
     int errType = 0;
 
-    TextEditingController hourController =
-        TextEditingController(text: chosenHour);
-    TextEditingController minuteController =
-        TextEditingController(text: chosenMinute);
+    TextEditingController hourController = TextEditingController(text: chosenHour);
+    TextEditingController minuteController = TextEditingController(text: chosenMinute);
 
     if(widget.intention > 1){
       chosenFile = widget.contextWallpaper.filePath;
@@ -349,111 +334,133 @@ class _CreateApp extends State<CreateApp> {
 
     Widget dayButtonsSection = Container(
       padding: EdgeInsets.all(16),
+
       child: Row(
+
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+
           Spacer(flex: 12),
-          OutlinedButton(
-            onPressed: null, //place function call within toggle button function
-            child: const Text("Sun", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null, //place function call within toggle button function
+          child: const Text("Sun", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+          
           Spacer(flex: 1),
-          OutlinedButton(
-            onPressed: null,
-            child: const Text("Mon", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null,
+          child: const Text("Mon", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+
           Spacer(flex: 1),
-          OutlinedButton(
-            onPressed: null,
-            child: const Text("Tue", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null, 
+          child: const Text("Tue", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+
           Spacer(flex: 1),
-          OutlinedButton(
-            onPressed: null,
-            child: const Text("Wed", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null, 
+          child: const Text("Wed", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+
           Spacer(flex: 1),
-          OutlinedButton(
-            onPressed: null,
-            child: const Text("Thu", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null, 
+          child: const Text("Thu", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+
           Spacer(flex: 1),
-          OutlinedButton(
-            onPressed: null,
-            child: const Text("Fri", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null, 
+          child: const Text("Fri", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+
           Spacer(flex: 1),
-          OutlinedButton(
-            onPressed: null,
-            child: const Text("Sat", style: TextStyle(fontSize: 11)),
-            style: dayToggleButton,
+
+          OutlinedButton(onPressed: null, 
+          child: const Text("Sat", style: TextStyle(fontSize: 11)),
+          style: dayToggleButton,
           ),
+
           Spacer(flex: 12),
+
         ],
       ),
+
     );
 
     Widget timeAndWeather = Container(
       padding: EdgeInsets.all(16),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Spacer(flex: 9),
-        Container(
-            // Time Input Box
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 2),
+
+      child: Row(
+        
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+
+            Spacer(flex: 9),
+
+            Container( // Time Input Box
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+              ),
+                child: Expanded(
+                  child: Row( 
+                  mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container( // Hour text
+                        width: 40,
+                        child: TextFormField(onChanged: null,
+                        textAlign: TextAlign.right, // convert to time values, invalid times should be reset.
+                        maxLength: 2,
+                        controller: hourController,
+                        decoration: InputDecoration(
+                          labelText: "Hour",
+                        ),), 
+                      ),
+
+                      const Text(":"),
+
+                      Container( // Minute text
+                        width: 40,
+                        child: TextFormField(onChanged: null , 
+                        textAlign: TextAlign.left, // convert to time values, invalid times should be reset.
+                        maxLength: 2,
+                        controller: minuteController,
+                        decoration: InputDecoration(
+                          labelText: "Min",
+                        ),), 
+                      ),
+                    ]
+                  ),
+                )
+              ),
+            Spacer(flex: 1),
+
+            AmPmToggle(),
+
+            Spacer(flex: 6),
+
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+              ),
+
+              child: WeatherDropMenu(),
+            
             ),
-            child: Expanded(
-              child:
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Container(
-                  // Hour text
-                  width: 40,
-                  child: TextFormField(
-                    onChanged: null,
-                    textAlign: TextAlign
-                        .right, // convert to time values, invalid times should be reset.
-                    maxLength: 2,
-                    controller: hourController,
-                    decoration: InputDecoration(
-                      labelText: "Hour",
-                    ),
-                  ),
-                ),
-                const Text(":"),
-                Container(
-                  // Minute text
-                  width: 40,
-                  child: TextFormField(
-                    onChanged: null,
-                    textAlign: TextAlign
-                        .left, // convert to time values, invalid times should be reset.
-                    maxLength: 2,
-                    controller: minuteController,
-                    decoration: InputDecoration(
-                      labelText: "Min",
-                    ),
-                  ),
-                ),
-              ]),
-            )),
-        Spacer(flex: 1),
-        AmPmToggle(),
-        Spacer(flex: 6),
-        Container(
-          height: 80,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 2),
-          ),
-          child: WeatherDropMenu(),
-        ),
-        Spacer(flex: 9),
-      ]),
+
+            Spacer(flex: 9),
+      ]
+      ),
     );
 
     Container buttonMenu() {
@@ -461,19 +468,18 @@ class _CreateApp extends State<CreateApp> {
       padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 32),    
       child: Row(
         children: [
-          OutlinedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            }, //function here to switch back to main menu
-            child: const Text("Back"),
-            style: ButtonStyle(
-              padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.all(32)),
-              backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
-              side: MaterialStatePropertyAll<BorderSide>(
-                BorderSide(color: Colors.black, width: 2),
-              ),
-            ),
-          ),
+          OutlinedButton(onPressed: () {
+
+                    Navigator.pop(context);
+
+                    },  //function here to switch back to main menu
+                    child: const Text("Back"),
+                    style: ButtonStyle(
+                      padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.all(32)),
+                      backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+                      side: MaterialStatePropertyAll<BorderSide>(BorderSide(color: Colors.black, width: 2),),
+                    ),
+                    ),
           Spacer(),
           OutlinedButton(onPressed: () {
                     if(checkFields(hourController.text, minuteController.text, chosenFile, chosenCond)){
@@ -519,8 +525,8 @@ class _CreateApp extends State<CreateApp> {
             )
           ],
         ),
-      ),
-    );
+        ),
+      );
   }
 
 }
