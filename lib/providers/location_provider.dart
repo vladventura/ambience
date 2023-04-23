7d08a345fd6c2540a8543da24e7e1642514e5d29
Vladimir Ventura <vladventura@outlook.es>
2023-04-23 15:54:39 -0400

    Adding location information store
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

---
+++ lib/providers/location_provider.dart
@@ -0,0 +1,13 @@
+import 'package:ambience/models/location_model.dart';
+import 'package:flutter/material.dart';
+
+class LocationProvider with ChangeNotifier {
+  LocationModel? _foundLocation;
+
+  LocationModel? get location => _foundLocation;
+
+  void setLocation(Map<String, dynamic> incoming) {
+    _foundLocation = LocationModel.fromJson(incoming);
+    notifyListeners();
+  }
+}
