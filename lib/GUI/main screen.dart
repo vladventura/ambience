7dacaae74b086f37b8319e5e69108714113ac54d
Vladimir Ventura <vladventura@outlook.es>
2023-04-23 19:03:14 -0400

    Made Drawer opened by FAB for re-location and logout.
    Light refactoring.
    
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- lib/GUI/main screen.dart
+++ lib/GUI/main screen.dart
@@ -1,7 +1,3 @@
-// TO DO:
-//  Add location gear button
-//  Add logout button
-
 import 'package:ambience/GUI/create.dart';
 import 'package:ambience/GUI/list.dart';
 import 'package:ambience/providers/location_provider.dart';
@@ -58,61 +54,96 @@ String getLocation() {
   return location;
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FloatingDrawerButton extends StatelessWidget {
+  const FloatingDrawerButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("Inside main screen");
-    debugPrint(context.read<LocationProvider>().location?.toJson().toString());
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      child: FloatingActionButton(
+        onPressed: () {
+          Scaffold.of(context).openDrawer();
+        },
+        child: const Icon(Icons.more_vert),
+      ),
+    );
+  }
+}
 
-    Widget weatherSection = Container(
-        padding: const EdgeInsets.all(32),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(Icons.sunny,
-                    size: 80,
-                    color: Colors
-                        .amber), // placeholder, attach function to icon to change based on weather
-                Text(
-                  checkTime(), // (TimeOfDay(hour: 12, minute: 02) !!! SCHEDULE TO UPDATE TIME EVERY MINUTE THROUGH A FUNCTION CALL !!!
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ), // placeholder, attach function to retrieve time
-                const Text("Boston, MA"), // placeholder, drop menu goes here
-              ],
-            ),
-          ],
-        ));
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-    Widget wallpaperSection = Expanded(
+  Widget wallpaperSection() {
+    return Expanded(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Padding(padding: EdgeInsets.only(left: 32)),
+          const Padding(
+            padding: EdgeInsets.only(left: 32),
+          ),
           checkWallpaper(),
-          const Padding(padding: EdgeInsets.only(right: 32)),
+          const Padding(
+            padding: EdgeInsets.only(right: 32),
+          ),
         ],
       ),
     );
+  }
 
-    Widget buttonMenu = Container(
+  Widget weatherSection() {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(Icons.sunny,
+                  size: 80,
+                  color: Colors
+                      .amber), // placeholder, attach function to icon to change based on weather
+              Text(
+                checkTime(), // (TimeOfDay(hour: 12, minute: 02) !!! SCHEDULE TO UPDATE TIME EVERY MINUTE THROUGH A FUNCTION CALL !!!
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ), // placeholder, attach function to retrieve time
+              const Text("Boston, MA"), // placeholder, drop menu goes here
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  ButtonStyle _buttonStyle({
+    MaterialStatePropertyAll<EdgeInsets> padding =
+        const MaterialStatePropertyAll<EdgeInsets>(
+      EdgeInsets.all(32),
+    ),
+    MaterialStatePropertyAll<Color> backgroundColor =
+        const MaterialStatePropertyAll<Color>(Colors.white),
+    MaterialStatePropertyAll<BorderSide> side =
+        const MaterialStatePropertyAll<BorderSide>(
+      BorderSide(color: Colors.black, width: 2),
+    ),
+  }) {
+    return ButtonStyle(
+      padding: padding,
+      backgroundColor: backgroundColor,
+      side: side,
+    );
+  }
+
+  Widget buttonMenu(BuildContext context) {
+    return Container(
       padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 32),
       child: Row(
         children: [
-          const OutlinedButton(
+          OutlinedButton(
             onPressed: null, //function to close program
-            style: ButtonStyle(
-              padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.all(32)),
-              backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
-              side: MaterialStatePropertyAll<BorderSide>(
-                BorderSide(color: Colors.black, width: 2),
-              ),
-            ),
-            child: Text("Quit"),
+            style: _buttonStyle(),
+            child: const Text("Quit"),
           ),
           const Spacer(),
           OutlinedButton(
@@ -120,48 +151,74 @@ class MainApp extends StatelessWidget {
               //function here to switch to list screen
               Navigator.pushNamed(context, '/List');
             },
-            style: const ButtonStyle(
-              padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.all(32)),
-              backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
-              side: MaterialStatePropertyAll<BorderSide>(
-                BorderSide(color: Colors.black, width: 2),
-              ),
-            ),
+            style: _buttonStyle(),
             child: const Text("List"),
           ),
           const Spacer(),
           OutlinedButton(
             onPressed: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CreateApp(
-                          contextWallpaper: WallpaperObj(),
-                          intention: 1,
-                          location: getLocation())));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CreateApp(
+                    contextWallpaper: WallpaperObj(),
+                    intention: 1,
+                    location: getLocation(),
+                  ),
+                ),
+              );
             }, //function here to switch to create screen
-            style: const ButtonStyle(
-              padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.all(32)),
-              backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
-              side: MaterialStatePropertyAll<BorderSide>(
-                BorderSide(color: Colors.black, width: 2),
-              ),
-            ),
+            style: _buttonStyle(),
             child: const Text("Create"),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("Inside main screen");
+    debugPrint(context.read<LocationProvider>().location?.toJson().toString());
 
     return MaterialApp(
       home: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            weatherSection,
-            wallpaperSection,
-            buttonMenu,
+            weatherSection(),
+            wallpaperSection(),
+            buttonMenu(context),
           ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+        floatingActionButton: const FloatingDrawerButton(),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              const DrawerHeader(
+                child: Text("Ambience"),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: const Text("Reset Location"),
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/LocationRequest');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                title: const Text("Log out"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
