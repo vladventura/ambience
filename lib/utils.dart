ac7330032d2535089dc37fcd864c1b651b007956
Vladimir Ventura <vladventura@outlook.es>
2023-04-24 23:02:45 -0400

    Adding helper functions
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,11 +1,42 @@
+import "dart:convert";
 import "dart:io";
-import "package:path/path.dart" as path;
+import "package:ambience/models/location_model.dart";
+import "package:flutter/material.dart";
+import "package:path/path.dart" as p;
+import 'package:ambience/constants.dart' as constants;
 import "package:path_provider/path_provider.dart";
 
 class Utils {
-  static Future<bool> createIfNotExists(String path) async {
-    Directory documentsDir = await getApplicationDocumentsDirectory();
+  static Future<void> saveToLocationFile(LocationModel incoming) async {
+    Directory dirToAmb = await createAmbienceIfNotExists();
+    File locationFile = File(p.join(dirToAmb.path, constants.locationFilename));
 
-    return false;
+    await locationFile.writeAsString(jsonEncode(incoming.toJson()));
+  }
+
+  static Future<LocationModel?> loadFromLocationFile() async {
+    Directory dirToAmb = await createAmbienceIfNotExists();
+    File locationFile = File(p.join(dirToAmb.path, constants.locationFilename));
+
+    if (!await locationFile.exists() || await locationFile.length() == 0) {
+      return null;
+    }
+
+    LocationModel model =
+        LocationModel.fromJson(jsonDecode(await locationFile.readAsString()));
+    return model;
+  }
+
+  static Future<Directory> createAmbienceIfNotExists() async {
+    Directory documentsDir = await getApplicationDocumentsDirectory();
+    String pathToAmbience = p.join(documentsDir.path, constants.appDataDirName);
+    Directory dirToAmb = Directory(pathToAmbience);
+    if (!await dirToAmb.exists()) {
+      await dirToAmb.create(recursive: true).catchError((e) {
+        debugPrint("Failed to create Ambience folder");
+        return dirToAmb;
+      });
+    }
+    return dirToAmb;
   }
 }
