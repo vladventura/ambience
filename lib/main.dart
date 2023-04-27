4e1944cd032fd81fb7936c4185a4f61825ebf7cd
Vladimir Ventura <vladventura@outlook.es>
2023-04-26 21:23:01 -0400

    Loading wallpaper object provider
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:ambience/GUI/create.dart';
 import 'package:ambience/models/weather_model.dart';
 import 'package:ambience/GUI/location_request.dart';
 import 'package:ambience/providers/location_provider.dart';
+import 'package:ambience/providers/wallpaper_obj_provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:ambience/handlers/file_handler.dart';
-import 'package:ambience/handlers/wallpaper_handler.dart';
 import 'package:ambience/weatherEntry/weather_entry.dart';
 import 'package:flutter/material.dart';
-import 'package:ambience/api/weather.dart';
 import "package:ambience/daemon/daemon.dart";
 import 'package:ambience/firebase/fire_handler.dart';
-import "package:ambience/GUI/create.dart";
 import "package:ambience/GUI/list.dart";
 import "package:ambience/GUI/login.dart";
 import "package:ambience/GUI/main screen.dart";
 import 'package:provider/provider.dart';
-import 'package:ambience/api/weather.dart';
 
 void main(List<String> args) async {
   await dotenv.load();
@@ -31,6 +28,9 @@ void main(List<String> args) async {
           providers: [
             ChangeNotifierProvider(
               create: (_) => LocationProvider(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => WallpaperObjProvider(),
             ),
           ],
           child: const MyApp(),
@@ -72,22 +72,27 @@ class _MyAppState extends State<MyApp> {
   }
 
   void asyncInit() async {
-    await context.read<LocationProvider>().loadLocation();
+    LocationProvider lp = context.read<LocationProvider>();
+    WallpaperObjProvider wop = context.read<WallpaperObjProvider>();
+    await lp.loadLocation();
+    await wop.loadWallpaperObjs();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const LoginApp(),
-          '/Home': (context) => const MainApp(),
-          '/List': (context) => ListApp(),
-          '/LocationRequest': (context) => const LocationRequest(),
-        });
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginApp(),
+        '/Home': (context) => const MainApp(),
+        '/List': (context) => const ListApp(),
+        '/LocationRequest': (context) => const LocationRequest(),
+        '/Create': (context) => const CreateApp(),
+      },
+    );
   }
 }
